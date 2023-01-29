a71ba444e6c0ab7a231723d2dc6732a3c65a7a71
kovid-sharma <kovid2020@gmail.com>
2023-01-29 12:16:24 +0530

    adding styled sheet

--- lib/src/screens/login.dart
+++ lib/src/screens/login.dart
@@ -2,6 +2,7 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'dart:io';
 import '../styles/textfields.dart';
+import '../styles/colors.dart';
 class Login extends StatelessWidget{
   @override
   Widget build(BuildContext context) {
@@ -19,14 +20,16 @@ class Login extends StatelessWidget{
 
   Widget pageBody(BuildContext context) {
     var height= MediaQuery.of(context).size.height*0.2;
-    var width= MediaQuery.of(context).size.width;
+    var width= MediaQuery.of(context).size.width*0.9;
     return ListView(
       padding: EdgeInsets.all(0.0),
       children: <Widget>[
         Container(
           height: height,
-          width:width,
+          width:width*1,
           child:Image.asset('assets/img_1.png',
+            // height: height,
+            // width:width*1,
           ),
         ),
         Container(
@@ -37,11 +40,18 @@ class Login extends StatelessWidget{
         ),
         Padding(
           padding: EdgeInsets.symmetric(
-              horizontal: TextFieldStyles.textBoxHorizontal(),
-              vertical: TextFieldStyles.textBoxVertical()),
+              horizontal: TextFieldStyles.textBoxHorizontal,
+              vertical: TextFieldStyles.textBoxVertical
+          ),
           child: email(),
         ),
-        password(),
+        Padding(
+          padding: EdgeInsets.symmetric(
+              horizontal: TextFieldStyles.textBoxHorizontal,
+              vertical: TextFieldStyles.textBoxVertical
+          ),
+          child: password(),
+        ),
       ],
     );
   }
@@ -50,7 +60,13 @@ class Login extends StatelessWidget{
     if (Platform.isIOS){
       return CupertinoTextField();
     } else {
-      return TextField();
+      return TextField(
+        keyboardType: TextInputType.emailAddress,
+        cursorColor: TextFieldStyles.cursorColor,
+        style:TextFieldStyles.text,
+        textAlign: TextFieldStyles.textAlign,
+        decoration: TextFieldStyles.materialDecoration('Email', Icons.email),
+      );
     }
   }
 
@@ -58,7 +74,13 @@ class Login extends StatelessWidget{
     if (Platform.isIOS){
       return CupertinoTextField();
     } else {
-      return TextField();
+      return TextField(
+        keyboardType: TextInputType.visiblePassword,
+        cursorColor: TextFieldStyles.cursorColor,
+        style:TextFieldStyles.text,
+        textAlign: TextFieldStyles.textAlign,
+        decoration: TextFieldStyles.materialDecoration('Password', Icons.lock_open_sharp),
+      );
     }
   }
 }
